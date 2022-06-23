2915241a830c38c1d7f15f31ad98b91e4ed5fc75
Abdul Alim Shohan <shohankhansk111@gmail.com>
2022-06-23 23:39:21 +0600

    funtion base

--- products/views.py
+++ products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -21,3 +23,33 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     # look_up_field ='pk
 
 # Product_detail_view = ProductDetailAPIView.as_view()
+
+@api_view(['GET','POST'])
+def product_alt_view(request,pk=None,*args,**kwargs):
+    method = request.method
+
+    if method == 'GET':
+        if pk is not None:
+            # detail View
+            obj = get_object_or_404(Product,pk = pk)
+            data = ProductSerializer(obj,many=False).data
+            return Response(data)
+        # list view
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+
+
+
+
+    if method == 'POST':
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid(raise_exception = True):
+            title = serializer.validation_data.get('title')
+            content = serializer.validation_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return  Response({"invalid": "Not good data"},status=400)
