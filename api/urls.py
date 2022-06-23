2ef8cb21ba3bf6e99234f499b13f1706b179aede
Abdul Alim Shohan <shohankhansk111@gmail.com>
2022-06-23 21:38:21 +0600

    APIView

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('home/', views.api_home),
+    path('', views.api_home),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self,serializer):
+        serializer.save()
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # look_up_field ='pk
+
+# Product_detail_view = ProductDetailAPIView.as_view()
