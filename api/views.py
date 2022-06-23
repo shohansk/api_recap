6a08cc386fdb3665b3816a4872ab791ac41a11cc
Abdul Alim Shohan <shohankhansk111@gmail.com>
2022-06-23 16:44:30 +0600

    add products app

--- api/views.py
+++ api/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def api_home(request, *args,**kwargs):
+    body = request.body
+    print(body)
     return JsonResponse({"message":"Hi there, this is shohan"})
--- api_recap/settings.py
+++ api_recap/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'api'
+# created app
+    'api',
+    'products'
 ]
 
 MIDDLEWARE = [
