fed1471821c339434e02ceece7874a661536bfa1
Abdul Alim Shohan <shohankhansk111@gmail.com>
2022-06-23 16:35:01 +0600

    create api app

---
+++ api/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import JsonResponse
+
+def api_home(request, *args,**kwargs):
+    return JsonResponse({"message":"Hi there, this is shohan"})
